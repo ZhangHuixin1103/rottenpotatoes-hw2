2ced62da4e9dc88ae7999602abc661a0827f9d37
Zhang Huixin <rzxyxxzhx1103@gmail.com>
2023-02-22 23:03:13 -0600

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,9 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Avatar', :rating => 'PG-13',
+    :release_date => '18-Dec-2009'}
 ]
 
 more_movies.each do |movie|
